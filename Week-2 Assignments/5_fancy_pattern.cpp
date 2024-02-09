a27003b572e74917f6573133257c9d1663586def
sajal <sajalsahu8@gmail.com>
2024-02-10 01:31:50 +0530

    updated

--- Week-2 Assignments/5_fancy_pattern.cpp
+++ Week-2 Assignments/5_fancy_pattern.cpp
@@ -9,7 +9,20 @@ int main()
     
     for(int i = 0 ; i < n; i++)
     {
-        
+        for(int j = 1; j < 2*n-1-i; j++){
+            cout<<" * ";
+        }
+
+        for(int j = 0; j < 2*(i+1) - 1; j++){
+            if(j%2 == 0){
+                cout<<" "<<i+1<<" ";
+            }
+            else cout<<" * ";
+        }
+
+       for(int j = 1; j < 2*n-1-i; j++){
+            cout<<" * ";
+        }
         cout<<endl;
     }
     return 0;
