f0bfd089ac439a4790ea5f652d7a03c019cd2c14
sajal <sajalsahu8@gmail.com>
2024-02-10 00:50:33 +0530

    updated

--- Week-2 Assignments/3_numeric_palindrome_eq_pyramid.cpp
+++ Week-2 Assignments/3_numeric_palindrome_eq_pyramid.cpp
@@ -9,9 +9,18 @@ int main()
     
     for(int i = 0 ; i < n; i++)
     {
-        for(int j = 0; j < 2*n-1; j++){
-            cout<<j+1;
+        for(int j = 0 ; j < n-1-i; j++){
+            cout<<" "<<" ";
         }
+
+        for(int j = 1; j <= i; j++){
+            cout<<j<<" ";
+        }
+
+        for(int j = i+1; j >= 1; j--){
+            cout<<j<<" ";
+        }
+        
         cout<<endl;
     }
     return 0;
