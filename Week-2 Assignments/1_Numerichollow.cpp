2a4c6762f07ecb8a71c4f7ad102194ec9cc5fbf3
sajal <sajalsahu8@gmail.com>
2024-02-09 22:32:03 +0530

    updates

--- Week-2 Assignments/1_Numerichollow.cpp
+++ Week-2 Assignments/1_Numerichollow.cpp
@@ -10,8 +10,7 @@ int main()
     for(int i = 0 ; i < n ; i++){
         for(int j = 0; j <= i; j++)
         {
-            if(j == 0 || j == i || i == n-1) cout<<j+1;   
-           
+            if(j == 0 || j == i || i == n-1) cout<<j+1;  
             else cout<<" ";
         }
         cout<<endl;
