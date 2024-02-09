3942ab01d50fbd17098e4facb62da5276718bbc1
sajal <sajalsahu8@gmail.com>
2024-02-09 22:43:01 +0530

    updated

--- Week-2 Assignments/2_invertedhollow.cpp
+++ Week-2 Assignments/2_invertedhollow.cpp
@@ -6,7 +6,13 @@ int main()
 {
     int n;
     cin>>n;
-
-    
+    for(int i = 0; i < n ; i++){
+        for(int j = i; j < n ; j++)
+        {
+            if(i == j || j == n-1 || i == 0) cout<<j+1;
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
