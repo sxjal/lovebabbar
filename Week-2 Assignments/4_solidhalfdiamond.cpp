346abaaee0bdbe1b989f28a0c0d639d9fdfccef5
sajal <sajalsahu8@gmail.com>
2024-02-10 01:05:39 +0530

    updated

--- Week-2 Assignments/4_solidhalfdiamond.cpp
+++ Week-2 Assignments/4_solidhalfdiamond.cpp
@@ -6,10 +6,21 @@ int main()
 {
     int n;
     cin>>n;
-    
-    for(int i = 0 ; i < n; i++)
+    int k = n-1;
+    for(int i = 0 ; i < 2*n-1; i++)
     {
-        
+        if(i < n){
+            for(int j = 0; j < i+1; j++){
+                cout<<"*";
+            }
+        }
+        else{
+            for(int j = 1; j <= k; j++){
+                cout<<"*";
+            }
+            k--;
+        }
+
         cout<<endl;
     }
     return 0;
