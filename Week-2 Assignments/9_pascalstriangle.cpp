fcd34d9ec09dc844d0647f840057be06a5a200f6
sajal <sajalsahu8@gmail.com>
2024-02-10 17:26:50 +0530

    pyscals triangle

---
+++ Week-2 Assignments/9_pascalstriangle.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 6;
+   
+
+    for(int i = 1; i<= n; i++){
+        int count=1;
+        for(int j = 1; j <=i ;j++ ){
+            cout<<count<<" ";
+            count = count*(i-j)/j;
+        }
+        cout<<endl;
+
+       
+   }
+    
+    return 0;
+}
