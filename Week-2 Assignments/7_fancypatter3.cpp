0e6e552dde145f98ff7550f11256a9d382cf978d
sajal <sajalsahu8@gmail.com>
2024-02-10 16:14:02 +0530

    done

--- Week-2 Assignments/7_fancypatter3.cpp
+++ Week-2 Assignments/7_fancypatter3.cpp
@@ -4,23 +4,19 @@ using namespace std;
 
 int main()
 {
-    int n = 3;
+    int n = 5;
   
+    //growing phase
     for(int i = 0; i < 2*n-1 ; i++){
-        if( i <= (2*n-1)/2){
-            for(int j = 0; j < i; j++){
-                 cout<<j+1<<" ";
-            }
-            for(int j = i; j >= 0; j--){
+        for(int j = 0; j < i; j++){
                 cout<<j+1<<" ";
-            }
-            cout<<endl;
         }
-        else{
-            // for(){
-
-            // }
+        for(int j = i; j >= 0; j--){
+            cout<<j+1<<" ";
         }
+        cout<<endl;     
     }
+
+
     return 0;
 }
