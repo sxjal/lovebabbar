80c90cebbd8b996b0fabaf25af46be2061f91d1e
sajal <sajalsahu8@gmail.com>
2024-02-10 17:42:17 +0530

    butterflydone

---
+++ Week-2 Assignments/10_butterflypattern.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 5;
+
+    for(int i = 1; i<= n; i++){
+        //first part
+        for(int j = 1; j<=i; j++)
+        {
+            cout<<"*";
+        }
+
+        //space
+        for(int j = 1; j <= (n-i)*2 ; j++)
+        {
+            cout<<" ";
+        }
+        //endpart
+        for(int j = 1; j <= i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+   }
+
+   for(int i = n; i >= 1; i--)
+   {
+        for(int j = i; j>=1 ; j--){
+            cout<<"*";
+        }
+
+        for(int j = 1; j<= (n-i)*2; j++){
+            cout<<" ";
+        }
+        
+        for(int j = i; j>=1 ; j--){
+            cout<<"*";
+        }
+        cout<<endl;
+   }
+    
+    return 0;
+}
