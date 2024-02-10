3485c56bf89b5c92b613db4d23fd766f20b079f0
sajal <sajalsahu8@gmail.com>
2024-02-10 15:56:45 +0530

    updated

--- Week-2 Assignments/6_fancypattern.cpp
+++ Week-2 Assignments/6_fancypattern.cpp
@@ -4,33 +4,31 @@ using namespace std;
 
 int main()
 {
-    int n = 5;
-    
+    int n = 4;
     int k = 1;
-    for(int i = 0 ; i < 9; i++)
-    {
-        if(i<n-1){
-            for(int j = 0; j<2*i+1; j++)
-            {
-                if(j%2 == 0){
-                    cout<<k;
-                    k++;
-                }
-                else cout<<"*";
-            }
-        }
 
-        else{
-           for(int j = 0; j<2*i+1; j++)
-            {
-                if(j%2 == 0){
-                    cout<<k;
-                    k--;
-                }
-                else cout<<"*";
-            }
-        }         
+    for(int i = 0 ; i <= n; i++)
+    {
+        for(int j = 0; j<i; j++)
+        {
+            cout<<k<<" ";
+            k++;       
+        }
         cout<<endl;
     }
+
+    int start = k-n;
+    for(int i = 0 ; i < n; i++)
+    {
+        k = start;
+        for(int j = 0; j<=n-i-1; j++)
+        {
+            cout<<k<<" ";
+            k++;
+        }
+        start = start - (n - i - 1);
+        cout<<endl;
+    }
+
     return 0;
 }
