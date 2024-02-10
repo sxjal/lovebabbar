aa2f4fb935e4a71198c0bfd92247d88b9b42b01e
sajal <sajalsahu8@gmail.com>
2024-02-10 19:22:48 +0530

    done

---
+++ Arrays Week 2/reverseanarray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[6];
+
+    for(int i = 0 ; i<6; i++){
+        cin>>arr[i];
+    }
+    
+    int pointer = 5;
+    for(int i = 0; i< 6; i++)
+    {
+        cout<<arr[i]<<" "<<arr[pointer--]<<endl;
+        int temp = arr[i];
+        arr[i] = arr[pointer];
+        arr[pointer] = temp;
+    }
+
+    for(int i = 0 ; i<6; i++){
+        cout<<arr[i];
+    }
+    
+    
+    return 0;
+}
