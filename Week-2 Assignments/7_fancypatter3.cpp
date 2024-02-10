0242238a39dafe9490950caf680e0e2d890ed207
sajal <sajalsahu8@gmail.com>
2024-02-10 16:12:09 +0530

    growing

--- Week-2 Assignments/7_fancypatter3.cpp
+++ Week-2 Assignments/7_fancypatter3.cpp
@@ -4,10 +4,23 @@ using namespace std;
 
 int main()
 {
-    int n = 4;
+    int n = 3;
   
-    for(int i = 0; i < n ; i++){
+    for(int i = 0; i < 2*n-1 ; i++){
+        if( i <= (2*n-1)/2){
+            for(int j = 0; j < i; j++){
+                 cout<<j+1<<" ";
+            }
+            for(int j = i; j >= 0; j--){
+                cout<<j+1<<" ";
+            }
+            cout<<endl;
+        }
+        else{
+            // for(){
 
+            // }
+        }
     }
     return 0;
 }
