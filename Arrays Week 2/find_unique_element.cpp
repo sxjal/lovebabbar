4e6a3052fecd5933f9ffcc4d7d029f9451d96295
sajal <sajalsahu8@gmail.com>
2024-02-11 22:57:18 +0530

    updated

--- Arrays Week 2/find_unique_element.cpp
+++ Arrays Week 2/find_unique_element.cpp
@@ -10,24 +10,23 @@ int main()
     cout<<"\n Enter Value: ";
     cin>>n;
 
-    vector<int> arr;
+ 
+    vector<int> arr(n);
 
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
 
-    arr.push_back(10);
-    arr.push_back(20);
+    int ans = 0;
 
-    cout<<arr[0];
-    cout<<arr[1];
-    // cout<<arr[2];
-    
-    cout<<"\n size: "<<arr.size();
-    cout<<"\n Capacity: "<<arr.capacity();
-    
-    arr.pop_back();
-    cout<<"\n size: "<<arr.size();
-    cout<<"\n Capacity: "<<arr.capacity();
+    for(int i = 0; i < n; i++)
+    {
+        ans = ans ^ arr[i];
+    }
 
+    cout<<ans;
 
-    
     return 0;
 }
+
+
