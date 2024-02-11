536df8be0f5f0a8b2aed95c033d3f76a6e318e48
sajal <sajalsahu8@gmail.com>
2024-02-11 23:30:50 +0530

    updated

--- Arrays Week 2/sort_0_1.cpp
+++ Arrays Week 2/sort_0_1.cpp
@@ -5,23 +5,27 @@ using namespace std;
 
 int main()
 {
-    vector<int> a{10,20,30,40,50};
-    
-    int sum = 80;
+    vector<int> a{0,1,1,0,1,0,1,0,0};
 
-    int flag = 0;
-    for(int i = 0; i<a.size(); i++){
-        for(int j = i; j <a.size(); j++){
-            for(int k = j; k<a.size(); k++){
-            if(a[i] + a[j] + a[k] == sum)
-            {
-                cout<<"Num 1: "<<a[i]<<" Num 2: "<<a[j]<<" Num 3: "<<a[k]<<endl;
-                break;
-            }
-            }
+
+    int start = 0;
+    int end = a.size()-1;
+
+    for(int value = 0; value<a.size(); value++){
+        if(a[value] == 1){
+            swap(a[value],a[end--]);
+            continue;
         }
+        else if(a[value] == 0){
+            swap(a[value],a[start--]);
+            continue;
+        }
+       
     }
- 
+
+    for( auto value:a){
+        cout<<value<<" ";
+    }
     
     return 0;
 }
